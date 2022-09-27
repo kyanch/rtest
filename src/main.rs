c45cf698a09908a65e001dbc5a33993e98d91616
kyanch <kyanc.dev@outlook.com>
2022-09-27 20:01:01 +0800

    new hash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{hash::{Hash, Hasher}, collections::hash_map::DefaultHasher};
+
+#[derive(Hash)]
+struct Test {
+    name:String
+}
+impl Test {
+    pub fn new(s:&str)->Self{
+        Test { name: s.to_string() }
+    }
+    pub fn calculate_hash(self:&Self)->u64{
+        // 这里指定 hash 函数
+        let mut s= DefaultHasher::new();
+        self.hash(&mut s);
+        s.finish()
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let t= Test::new("abc");
+    print!("{}",t.calculate_hash());
 }
